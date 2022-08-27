1c7cd9f1bfe71380c31138f5ee84b3d24be91397
Dmitry Romanov <me@wtrn.ru>
2022-08-27 13:27:13 +0300

    Init

---
+++ src/main/java/ru/wtrn/yeelightmqtt/YeelightMqttApplication.java
@@ -0,0 +1,13 @@
+package ru.wtrn.yeelightmqtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeelightMqttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeelightMqttApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/wtrn/yeelightmqtt/YeelightMqttApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.wtrn.yeelightmqtt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeelightMqttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
