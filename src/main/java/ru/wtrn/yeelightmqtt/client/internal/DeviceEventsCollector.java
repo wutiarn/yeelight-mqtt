4d3bbaecfcf4225aeeffe654e4029e2dd5217f3f
Dmitry Romanov <me@wtrn.ru>
2022-08-27 19:00:29 +0300

    WIP: Implement DeviceEventsCollector

--- src/main/java/ru/wtrn/yeelightmqtt/client/internal/DeviceEventsCollector.java
+++ src/main/java/ru/wtrn/yeelightmqtt/client/internal/DeviceEventsCollector.java
@@ -1,7 +1,42 @@
 package ru.wtrn.yeelightmqtt.client.internal;
 
-public class DeviceEventsCollector {
-    public void onNewEvent(String event) {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.collect.MapMaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class DeviceEventsCollector {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final Logger logger = LoggerFactory.getLogger(DeviceEventsCollector.class);
+
+    private final Cache<Integer, JsonNode> eventsCache = CacheBuilder.newBuilder()
+            .maximumSize(100)
+            .expireAfterWrite(1, TimeUnit.MINUTES)
+            .build();
+
+    public DeviceEventsCollector() {
+
+    }
+
+    public void onNewEvent(String event) {
+        JsonNode eventNode;
+        try {
+            eventNode = objectMapper.readTree(event);
+        } catch (JsonProcessingException e) {
+            logger.error("Failed to read event to JsonNode: {}", event);
+            return;
+        }
+        JsonNode eventId = eventNode.get("id");
+        if (eventId != null && eventId.isNumber()) {
+            eventsCache.put(eventId.intValue(), eventNode);
+        }
     }
 }
